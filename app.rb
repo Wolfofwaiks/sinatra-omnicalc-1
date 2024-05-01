621040401de575dc3a3841df405bc61a2c395b8f
Wolfofwaiks <165302970+Wolfofwaiks@users.noreply.github.com>
2024-05-01 14:13:02 +0000

    first 2 tabs built

--- app.rb
+++ app.rb
@@ -1,13 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/howdy") do 
-  erb(:hello)
-end 
-  
-get ("/goodbye") do 
-  erb(:bye)
-end 
 
 get ("/square/new") do 
   erb(:new_square)
@@ -26,9 +19,13 @@ erb(:homepage)
 end
 
 get ("/square_root/new") do
+  erb(:new_square)
+end
+
+get ("/square_root/results") do
   @the_num = params.fetch("users_number").to_f
 
-  @the_result = @the_num ** 2
+  @the_result = Math.sqrt(@the_num)
 
-  erb(:square_results)
+  erb(:square_root_results)
 end
